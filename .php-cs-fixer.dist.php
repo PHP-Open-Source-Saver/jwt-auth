6ddde70155fd9bf7fc0ef65c044837028d1198b0
Leonid Meleshin <leon.03.99@gmail.com>
2021-11-24 17:30:42 +0300

    chore: update license in sources

--- .php-cs-fixer.dist.php
+++ .php-cs-fixer.dist.php
@@ -1,14 +1,33 @@
-<?php declare(strict_types=1);
+<?php
+
+declare(strict_types=1);
 
 use PhpCsFixer\Config;
 
 require __DIR__ . '/vendor/autoload.php';
 
 $finder = PhpCsFixer\Finder::create()
-    ->in(__DIR__);
+    ->in([
+             __DIR__ . '/src',
+             __DIR__ . '/tests',
+         ]);
 
 return (new Config())
     ->setFinder($finder)
     ->setRules([
-        '@PSR12' => true,
-    ]);
+                   '@PSR12' => true,
+                   'header_comment' => [
+                       'comment_type' => 'comment',
+                       'header' => <<<HEREDOC
+This file is part of jwt-auth.
+
+(c) 2014-2021 Sean Tymon <tymon148@gmail.com>
+(c) 2021 PHP Open Source Saver
+
+For the full copyright and license information, please view the LICENSE
+file that was distributed with this source code.
+HEREDOC,
+                       'location' => 'after_declare_strict',
+
+                   ]
+               ]);
--- tests/DefaultConfigValuesTest.php
+++ tests/DefaultConfigValuesTest.php
@@ -3,6 +3,9 @@
 /*
  * This file is part of jwt-auth.
  *
+ * (c) Sean Tymon <tymon148@gmail.com>
+ * (c) PHP Open Source Saver
+ *
  * For the full copyright and license information, please view the LICENSE
  * file that was distributed with this source code.
  */
